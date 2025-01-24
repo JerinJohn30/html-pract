475f2d09e0a3cb8068214971e1e318f83f97889e
JerinJohn30 <jerinjohn3010@gmail.com>
2025-01-24 10:09:34 +0530

    Add files via upload

---
+++ SportsItem.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export class SportsItem extends Component{
+    constructor(props){
+        super(props);
+    }
+    state = {};
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+}
---
+++ SportsList.jsx
@@ -0,0 +1,50 @@
+import { Component } from "react";
+import { SportsItem } from "./SportsItem";
+
+export class SportsList  extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {
+        allSports:[
+            {
+                sportId:101,
+                sportName:"Cricket",
+                sportEmoji:"🏏"
+            },
+            {
+                sportId:102,
+                sportName:"Football",
+                sportEmoji:"⚽"
+            },
+            {
+                sportId:103,
+                sportName:"Hockey",
+                sportEmoji:"🏑"
+            }
+        ]
+    }
+    mappedAllSports=allSports.map((eachSports)=>{
+        <SportsItem
+            key={eachSports.sportId}
+            item={eachSports}
+            removefn={(sportId)=>deleteSports(sportId)}
+        ></SportsItem>
+    });
+    deleteSports(sId){
+        this.state.allSports=this.state.allSports.filter((eachSports)=>eachSports.sportId!=sId);
+    }
+    render() { 
+        return (
+            <>
+                <div className="conatiner m-5">
+                    <h3>List Of Sports</h3>
+                    <ul className="list-group">
+                        {this.mappedAllSports}
+                    </ul>
+                </div>
+            </>
+        );
+    }
+}
+ 
